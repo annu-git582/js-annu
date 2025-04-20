449ca0a8cdb6b80d2d6907e97de970a9ee46ff52
annu-git582 <anushkachaubey70@gmail.com>
2025-04-20 08:45:48 +0000

    objects are here

---
+++ .devcontainer/object.js
@@ -0,0 +1,11 @@
+
+/* though the help of constructo {
+object . create} */
+const JsUser = {
+    name: "anushka chaubey",
+    rollno:"59",
+    age : "18",
+    email : "anushkachaubey70@gmail.com",
+}
+console.log(JsUser.name)
+console.log(JsUser["email"])
