6064c4cfa8c443217fc467fdf1542a397e0e6fcc
annu-git582 <anushkachaubey70@gmail.com>
2025-04-20 12:55:05 +0000

    json and javascript study

--- .devcontainer/object.js
+++ .devcontainer/object.js
@@ -1,11 +1,32 @@
-
+//********OBJECT LITERALS******************/
 /* though the help of constructo {
 object . create} */
-const JsUser = {
+/*const JsUser = {
     name: "anushka chaubey",
     rollno:"59",
     age : "18",
     email : "anushkachaubey70@gmail.com",
+}*/
+//console.log(JsUser.name)
+//console.log(JsUser[email])
+//chanege  the value of object 
+//JsUser.email = "annu@gmail.com"
+//value lock(freeze)
+//Object.freeze(JsUser)
+/********************OBJECT CONSTRUCTOR*************/
+
+const TinderUser = {}
+    TinderUser.id="123abc",
+    TinderUser.name="anuu",
+    TinderUser.isloggedIn="False"
+//console.log(TinderUser)
+const Regularobject = { //object ke andr object
+    email : "anushkachaubey70@gmail.com",
+    userfullname : {
+        Firstname :"Anushka",
+        Lastname : "chaubey",
+    },
+    id :"anuchaubey"
 }
-console.log(JsUser.name)
-console.log(JsUser["email"])
+console.log(Regularobject.userfullname)
+
